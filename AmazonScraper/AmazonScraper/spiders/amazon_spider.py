c70df7609948397bc9dbe24d6749d053b3f0687d
AlanVazquez99 <alanvazquez1999@gmail.com>
2020-05-05 20:11:37 -0500

    Fixed issue with 'price_original' that was same of 'price_current' And also export to CSV

--- AmazonScraper/AmazonScraper/spiders/amazon_spider.py
+++ AmazonScraper/AmazonScraper/spiders/amazon_spider.py
@@ -2,10 +2,21 @@
 import scrapy
 from ..items import AmazonscraperItem
 
+import datetime
+
 class AmazonSpider(scrapy.Spider):
     name = 'amazon'
     # allowed_domains = ['amazon.com.mx']
     start_urls = ['https://www.amazon.com.mx/laptop-Laptops-Computadoras-Componentes-y-Accesorios/s?k=laptop&rh=n%3A10189669011']
+
+    custom_settings = {
+        'FEED_URI': 'amazon' + datetime.datetime.today().strftime('%m%d') + '.csv',
+        'FEED_FORMAT': 'csv',
+        'FEED_EXPORTERS': {
+            'csv': 'scrapy.exporters.CsvItemExporter',
+        },
+        'FEED_EXPORT_ENCODING': 'utf-8',
+    }
 
     def parse(self, response):
         items = AmazonscraperItem()
@@ -15,6 +26,6 @@ class AmazonSpider(scrapy.Spider):
 
             items['name'] = product.css('.a-color-base.a-text-normal::text').get()
             items['price_current'] = product.css('.a-price-whole::text').get()
-            items['price_original'] = product.css('.a-offscreen::text').get()
+            items['price_original'] = product.css('.a-text-price .a-offscreen::text').get()
 
             yield items
